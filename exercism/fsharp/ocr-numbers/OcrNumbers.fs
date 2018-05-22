fc4907a6a7661394b7fe446984ad8b9fa2571d2e
Zlatozar Zhelyazkov <z_zhelyazkov@prosyst.bg>
2018-05-22 17:45:50 +0300

    Second attempt - add effects

--- exercism/fsharp/ocr-numbers/OcrNumbers.fs
+++ exercism/fsharp/ocr-numbers/OcrNumbers.fs
@@ -13,6 +13,23 @@ let decodeDigit pix =
     | [" _ ";"|_|";"|_|"] -> Some 8
     | [" _ ";"|_|";" _|"] -> Some 9
     | _                   -> None
+
+let checkOcrScan lst=
+    if (List.length lst % 4 = 0) then Some lst
+    else None
+
+let rec sliceLines lst =
+    if List.length lst = 0 then []
+    else 
+        [lst.[0..3]] @ (sliceLines lst.[4..])
+   
+// checkOcrScan ["    "] |> Option.map sliceLines |> ignore
+   
+let checkOcrDigit lst =
+    let ocrSize = 
+        List.exists (fun elm -> not (String.length elm % 3 = 0)) lst
+    if (ocrSize) then None
+    else Some lst
      
 let composeDigits lst =
     let rec composeDigit lst =
@@ -21,10 +38,7 @@ let composeDigits lst =
            [lst.[0..2]] @ (composeDigit lst.[3..])
     List.map composeDigit lst
     
-let rec formLines lst =
-    if List.length lst = 0 then []
-    else 
-        [lst.[0..3]] @ (formLines lst.[4..])
+// checkOcrDigit ["   "; "  |"; "  |"; "   "] |> Option.map composeDigits |> ignore 
            
 let formatDigits (lst: string list list) =
     let numOfDigits = List.length lst.[0] - 1
@@ -34,12 +48,17 @@ let formatDigits (lst: string list list) =
             List.append [ [lst.[0].[idx]] @ [lst.[1].[idx]] @ [lst.[2].[idx]] ] (recognizeLine (idx + 1))
     recognizeLine 0 
 
+// checkOcrDigit ["   "; "  |"; "  |"; "   "] |> Option.map composeDigits |> Option.map formatDigits |> ignore
+
 let recognizeDigits digits =
-    digits
-        |> composeDigits
-        |> formatDigits
-        |> List.map decodeDigit
-            
+    checkOcrDigit digits
+        |> Option.map composeDigits
+        |> Option.map formatDigits
+        |> Option.map (List.map decodeDigit)
+
+// recognizeDigits [ "   "; "  |"; "  |"; "   " ] |> ignore    
+        
 let convert input =
-    formLines input
-        |> List.map recognizeDigits
+    checkOcrScan input
+        |> Option.map sliceLines
+        |> Option.map (List.map recognizeDigits)
