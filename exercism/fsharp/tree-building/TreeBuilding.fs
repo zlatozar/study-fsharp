261f5f957e8f49de8e9daeec9e502e1cd5b8f112
Zlatozar Zhelyazkov <z.zhelyazkov@prosyst.bg>
2019-12-05 10:32:19 +0200

    Finish 'tree-building' challenge

--- exercism/fsharp/tree-building/TreeBuilding.fs
+++ exercism/fsharp/tree-building/TreeBuilding.fs
@@ -36,101 +36,75 @@ type Tree =
                                compare t1.id t2.id
             | _             -> failwith "Cannot compare values of different types."
 
-type TreeBuilder =
-    { mutable hasRoot: bool; waitingParents: Dictionary<int, Tree list> }
-    with
-        static member empty = { hasRoot=false; waitingParents=new Dictionary<int, Tree list>() }
-
-let markIfRoot treeBuilder ({ RecordId=recId; ParentId=parentId } as record) =
-        if recId = 0 && parentId = 0 then treeBuilder.hasRoot <- true
-        record
-
-let validateRecord ({ RecordId=id; ParentId=parentId } as record) =
-    let idShouldBigger =
-        if (id < parentId) then
-            failwith (sprintf "Invalid record: %A. 'id' should be bigger than 'parentId'." record)
-
-    let idDifferParentId =
-        // except for root element
-        if (id <> 0 && parentId <> 0) then
-            if (id = parentId) then
-                failwith (sprintf "Direct cycle was detected for record: %A" record)
-
-    idShouldBigger; idDifferParentId
-    record
-
-let validateOutput treeBuilder =
-    let root =
-        if not treeBuilder.hasRoot then failwith "There is no root element."
-
-    let detached =
-        if treeBuilder.waitingParents.Count > 1 then failwith "There is detached branches."
-
-    root; detached
-
-[<RequireQualifiedAccess>]
-module TreeBuilder =
-
-    let rec private remove elm lst =
+let rec private remove elm lst =
         match lst with
         | h::t when h = elm -> t
         | h::t              -> h::remove elm t
         | _                 -> []
 
-    // Add in sorted set
-    let rec private add (node: Tree) (children: Tree list) =
-        match children with
-        | []       -> [node]
-        | hd :: tl -> if node.id = hd.id then hd :: tl
-                      else
-                          if node.id < hd.id then node :: hd :: tl
-                          else hd :: add node tl
+let rec private add (node: Tree) (children: Tree list) =
+    match children with
+    | []       -> [node]
+    | hd :: tl -> if node.id = hd.id then hd :: tl
+                    else
+                        if node.id < hd.id then node :: hd :: tl
+                        else hd :: add node tl
 
-    let private elements (d: Dictionary<int, Tree list>) =
-        seq {
-            for kv in d do
-                yield (kv.Key, kv.Value)
-        }
+let private elements (d: Dictionary<int, Tree list>) =
+    seq {
+        for kv in d do
+            yield (kv.Key, kv.Value)
+    }
 
-    // replace waitings for given index
-    let change treeBuilder idx (newBranch: Tree list) =
-        if treeBuilder.waitingParents.Remove idx then
-            treeBuilder.waitingParents.Add (idx, newBranch)
+// Domain logic
+
+let idOutOfRange max ({ RecordId=recId; ParentId=_ } as record) =
+    if recId >= max then
+        failwith (sprintf "Invalid record: %A. 'id' should be bigger than record set lenght." record)
+
+let idShouldBigger ({ RecordId=recId; ParentId=parentId } as record) =
+    if recId < parentId then
+        failwith (sprintf "Invalid record: %A. 'id' should be bigger than 'parentId'." record)
+
+let idDifferParentId ({ RecordId=recId; ParentId=parentId } as record) =
+    // except for root element
+    if recId <> 0 && parentId <> 0 && recId = parentId then
+        failwith (sprintf "Direct cycle was detected for record: %A" record)
+
+type TreeConstructor() =
+    let mutable hasRoot = false
+    let waitingParents = new Dictionary<int, Tree list>()
+
+    member private __.change idx (newBranch: Tree list) =
+        if waitingParents.Remove idx then
+            waitingParents.Add (idx, newBranch)
         else
             failwith (sprintf "Waitings do not include parentId=%i" idx)
 
-    let newBuilder = fun () -> TreeBuilder.empty
-
-    // waitings must conatain parentId
-    let group treeBuilder parentId (newLeaf: Tree) =
-        let waitings = treeBuilder.waitingParents.Item parentId
+    // waitings must conatain 'parentId'
+    member private this.group parentId (newLeaf: Tree) =
+        let waitings = waitingParents.Item parentId
 
         let newBranch = if parentId = 0 then
                             match waitings with
                             | []                      -> failwith (sprintf "Waiting with id=%i withwout children" parentId)
-                            | [Branch (id, children)] when id = 0 -> if newLeaf.id = 0 then
-                                                                         [Branch (0, ref waitings)]
-                                                                     else
-                                                                         [Branch (0, ref (add newLeaf !children))]
+                            | [Branch (id, children)] when id = 0
+                                                      -> if newLeaf.id = 0 then
+                                                             [Branch (0, ref waitings)]
+                                                          else
+                                                             [Branch (0, ref (add newLeaf !children))]
                             | [Leaf id] when id = 0   -> [Branch (0, ref [newLeaf])]
                             | _ as leaf               -> if newLeaf.id = 0 then
                                                              [Branch (0, ref leaf)]
                                                          else
-                                                             // wait for root
                                                              add newLeaf waitings
                         else
                             add newLeaf waitings
 
-        change treeBuilder parentId newBranch
+        this.change parentId newBranch
 
-    // waitings must contain recId
-    let wrap treeBuilder (wrapper: Tree) =
-        let waitings = treeBuilder.waitingParents.Item wrapper.id
-        Branch (wrapper.id, ref waitings)
-
-    // parentId must not be part of waiting ids
-    let placeLeaf treeBuilder parentId (newBranch: Tree) =
-
+    // 'parentId' must not be part of waiting ids
+    member private this.placeLeaf parentId (newBranch: Tree) =
         let rec innerLoop (parent: Tree option) idx children =
             match children with
             | []              -> false
@@ -140,11 +114,11 @@ module TreeBuilder =
                                      | Some p -> let withoutParent = remove leaf !p.children
                                                  p.children := add (Branch(id, ref [newBranch])) withoutParent
 
-                                     | None   -> let restBranch = treeBuilder.waitingParents.Item idx
+                                     | None   -> let restBranch = waitingParents.Item idx
                                                                       |> remove leaf
 
                                                  let branch = add (Branch (id, ref [newBranch])) restBranch
-                                                 change treeBuilder idx branch
+                                                 this.change idx branch
                                      true
 
                                  else if parentId < id then false
@@ -164,18 +138,56 @@ module TreeBuilder =
         Seq.tryFind (fun (idx, children) -> if idx = 0 || parentId < idx then
                                                 innerLoop None idx children
                                             else false)
-                        (elements treeBuilder.waitingParents) |> Option.isSome
+                        (elements waitingParents) |> Option.isSome
 
-    let rec containsId idx (branch: Tree) =
-        match branch with
-        | Leaf id           -> if idx = id then true else false
+    member private this.processRecId { RecordId=recId; ParentId=parentId } =
+        if waitingParents.ContainsKey recId then
+            let waitings = waitingParents.Item recId
+            let branch = Branch (recId, ref waitings)
 
-        | Branch (id, children)
-                            -> if idx = id then true
-                                   else if idx < id then false
-                                   else
-                                       List.tryFind (fun leaf -> containsId idx leaf) !children
-                                           |> Option.isSome
+            if recId = 0 then // place root record
+                this.change 0 [branch]
+                None
+            else
+                waitingParents.Remove recId |> ignore
+                Some (parentId, branch)
+        else
+            Some (parentId, Leaf recId)
+
+    member private this.processParentId (newBranch: (int*Tree) option) =
+        match newBranch with
+        | None                    -> ()
+        | Some (parentId, branch) -> if waitingParents.ContainsKey parentId then
+                                        this.group parentId branch
+
+                                     else
+                                        if this.placeLeaf parentId branch then
+                                            waitingParents.Remove parentId |> ignore
+                                        else
+                                            waitingParents.Add (parentId, [branch])
+
+    member private this.add ({ RecordId=recId; ParentId=parentId } as record) =
+        if recId = 0 && parentId = 0 then
+            if hasRoot = false then hasRoot <- true
+            else failwith "Root record is already placed"
+
+        record
+            |> this.processRecId
+            |> this.processParentId
+
+    member this.construct (records: Record list) =
+        let max = List.length records
+
+        let checkInput record =
+            idOutOfRange max record; idShouldBigger record; idDifferParentId record
+
+        List.iter (fun record -> checkInput record; this.add record) records
+
+        if not hasRoot then failwith "Tree has no root."
+        if waitingParents.Count > 1 then
+             failwith (sprintf "There is detached branches with keys: %A" waitingParents.Keys)
+        // resulted tree
+        waitingParents.[0].[0]
 
 // Helper functions
 
@@ -196,50 +208,4 @@ let children t =
 
 // Main
 let buildTree (records: Record list) :Tree =
-    if List.isEmpty records then failwith "Record list should not be empty."
-
-    let treeBuilder = TreeBuilder.empty
-
-    let placeRecId treeBuilder { RecordId=recId; ParentId=parentId } =
-        if treeBuilder.waitingParents.ContainsKey recId then
-            let branch = TreeBuilder.wrap treeBuilder (Leaf recId)
-            if recId = 0 then // place root record
-                TreeBuilder.change treeBuilder 0 [branch]
-                None
-            else
-                treeBuilder.waitingParents.Remove recId |> ignore
-                Some (parentId, branch)
-        else
-            Some (parentId, Leaf recId)
-
-    let checkIndirectCycles (placedRecord: (int*Tree) option) =
-        match placedRecord with
-        | None                    -> placedRecord
-        | Some (parentId, branch) -> if TreeBuilder.containsId parentId branch then
-                                         failwith "Indirect cycle"
-                                     else
-                                         placedRecord
-
-    let processParentId treeBuilder (newBranch: (int*Tree) option) =
-        match newBranch with
-        | None                    -> ()
-        | Some (parentId, branch) -> if treeBuilder.waitingParents.ContainsKey parentId then
-                                         TreeBuilder.group treeBuilder parentId branch
-
-                                     else
-                                        if TreeBuilder.placeLeaf treeBuilder parentId branch then
-                                            treeBuilder.waitingParents.Remove parentId |> ignore
-                                        else
-                                            treeBuilder.waitingParents.Add (parentId, [branch])
-
-    let buildTree record =
-        validateRecord record
-            |> markIfRoot treeBuilder
-            |> placeRecId treeBuilder
-            |> processParentId treeBuilder
-
-    List.iter buildTree records
-
-    validateOutput treeBuilder
-
-    treeBuilder.waitingParents.[0].[0]
+    TreeConstructor().construct records
--- exercism/fsharp/tree-building/TreeBuildingTest.fs
+++ exercism/fsharp/tree-building/TreeBuildingTest.fs
@@ -1,196 +1,13 @@
-// This file was created manually and its version is 1.0.0.
+// This file was created manually and its version is 2.0.0.
 
 module TreeBuildingTest
 
-open Xunit
-open FsUnit.Xunit
 open System
 
+open FsUnit.Xunit
+open Xunit
+
 open TreeBuilding
-
-[<Fact>]
-let ``Attach record first level waiting leaf`` () =
-    let t = [Leaf 1]
-
-    let treeBuilder = TreeBuilder.empty
-    treeBuilder.waitingParents.Add (10, t)
-
-    TreeBuilder.placeLeaf treeBuilder 1 (Leaf 20) |> should be True
-    treeBuilder.waitingParents.[10] |> should equal [Branch (1, ref [Leaf 20])]
-
-[<Fact>]
-let ``Attach record first level waiting branch`` () =
-    let t = [Branch (1, ref [Leaf 2])]
-
-    let treeBuilder = TreeBuilder.empty
-    treeBuilder.waitingParents.Add (10, t)
-
-    TreeBuilder.placeLeaf treeBuilder 1 (Leaf 20) |> should be True
-    treeBuilder.waitingParents.[10] |> should equal [Branch (1, ref [Leaf 2; Leaf 20])]
-
-[<Fact>]
-let ``Attach record in a deep leaf`` () =
-    let t = [Branch (0, ref [Leaf 1;  Branch (2, ref [Leaf 3])])]
-
-    let treeBuilder = TreeBuilder.empty
-    treeBuilder.waitingParents.Add (10, t)
-
-    TreeBuilder.placeLeaf treeBuilder 3 (Leaf 20) |> should be True
-    treeBuilder.waitingParents.[10] |> should equal [Branch (0, ref [Leaf 1;  Branch (2, ref [Branch (3, ref [Leaf 20])])])]
-
-[<Fact>]
-let ``Attach record in a deep branch`` () =
-    let t = [Branch (0, ref [Leaf 1;  Branch (2, ref [Leaf 3])])]
-
-    let treeBuilder = TreeBuilder.empty
-    treeBuilder.waitingParents.Add (10, t)
-
-    TreeBuilder.placeLeaf treeBuilder 2 (Leaf 20) |> should be True
-    treeBuilder.waitingParents.[10] |> should equal [Branch (0, ref [Leaf 1;  Branch (2, ref [Leaf 3; Leaf 20])])]
-
-[<Fact>]
-let ``Attach Leaf in a Leaf`` () =
-    let t = [Branch (0, ref [Leaf 1;  Branch (2, ref [Leaf 3])])]
-
-    let treeBuilder = TreeBuilder.empty
-    treeBuilder.waitingParents.Add (10, t)
-
-    TreeBuilder.placeLeaf treeBuilder 3 (Leaf 20) |> should be True
-    treeBuilder.waitingParents.[10] |> should equal [Branch (0, ref [Leaf 1;  Branch (2, ref [Branch (3, ref [Leaf 20])])])]
-
-[<Fact>]
-let ``Attach Leaf in a Branch`` () =
-    let t = [Branch (0, ref [Leaf 1;  Branch (2, ref [Leaf 3])])]
-
-    let treeBuilder = TreeBuilder.empty
-    treeBuilder.waitingParents.Add (10, t)
-
-    TreeBuilder.placeLeaf treeBuilder 3 (Branch (20, ref [Leaf 30])) |> should be True
-    treeBuilder.waitingParents.[10]
-        |> should equal [Branch (0, ref [Leaf 1;  Branch (2, ref [Branch (3, ref [Branch (20, ref [Leaf 30])])])])]
-
-[<Fact>]
-let ``Attach Branch in a Leaf`` () =
-    let t = [Branch (0, ref [Leaf 1;  Branch (2, ref [Leaf 3])])]
-
-    let treeBuilder = TreeBuilder.empty
-    treeBuilder.waitingParents.Add (10, t)
-
-    TreeBuilder.placeLeaf treeBuilder 2 (Leaf 20) |> should be True
-    treeBuilder.waitingParents.[10]
-        |> should equal [Branch (0, ref [Leaf 1;  Branch (2, ref [Leaf 3; Leaf 20])])]
-
-[<Fact>]
-let ``Attach Branch in a Branch`` () =
-    let t = [Branch (0, ref [Leaf 1;  Branch (2, ref [Leaf 3])])]
-
-    let treeBuilder = TreeBuilder.empty
-    treeBuilder.waitingParents.Add (10, t)
-
-    TreeBuilder.placeLeaf treeBuilder 2 (Branch (20, ref [Leaf 30])) |> should be True
-    treeBuilder.waitingParents.[10]
-        |> should equal [Branch (0, ref [Leaf 1;  Branch (2, ref [Leaf 3; Branch (20, ref [Leaf 30])])])]
-
-[<Fact>]
-let ``Attach in a first level leaf`` () =
-    let t = [Leaf 1]
-
-    let treeBuilder = TreeBuilder.empty
-    treeBuilder.waitingParents.Add (10, t)
-
-    TreeBuilder.placeLeaf treeBuilder 1 (Leaf 30) |> should be True
-    treeBuilder.waitingParents.[10]
-        |> should equal [Branch (1, ref [Leaf 30])]
-
-[<Fact>]
-let ``Attach record in a first level branch`` () =
-    let t = [Branch (1, ref [Leaf 2])]
-
-    let treeBuilder = TreeBuilder.empty
-    treeBuilder.waitingParents.Add (10, t)
-
-    TreeBuilder.placeLeaf treeBuilder 1 (Leaf 30) |> should be True
-    treeBuilder.waitingParents.[10]
-        |> should equal [Branch (1, ref [Leaf 2; Leaf 30])]
-
-[<Fact>]
-let ``Waiting for root`` () =
-    let t = [Leaf 1]
-
-    let treeBuilder = TreeBuilder.empty
-    treeBuilder.waitingParents.Add (0, t)
-
-    TreeBuilder.group treeBuilder 0 (Leaf 2)
-    treeBuilder.waitingParents.[0] |> should equal [Leaf 1; Leaf 2]
-
-[<Fact>]
-let ``Root record already there`` () =
-    let t = [Leaf 0]
-
-    let treeBuilder = TreeBuilder.empty
-    treeBuilder.waitingParents.Add (0, t)
-
-    TreeBuilder.group treeBuilder 0 (Leaf 1)
-    treeBuilder.waitingParents.[0] |> should equal [Branch (0, ref [Leaf 1])]
-
-[<Fact>]
-let ``Root record already there and has children`` () =
-    let t = [Branch (0, ref [Leaf 2])]
-
-    let treeBuilder = TreeBuilder.empty
-    treeBuilder.waitingParents.Add (0, t)
-
-    TreeBuilder.group treeBuilder 0 (Leaf 1)
-    treeBuilder.waitingParents.[0] |> should equal [Branch (0, ref [Leaf 1; Leaf 2])]
-
-[<Fact>]
-let ``Root record comes later`` () =
-    let t = [Leaf 1]
-
-    let treeBuilder = TreeBuilder.empty
-    treeBuilder.waitingParents.Add (0, t)
-
-    TreeBuilder.group treeBuilder 0 (Leaf 0)
-    treeBuilder.waitingParents.[0] |> should equal [Branch (0, ref [Leaf 1])]
-
-[<Fact>]
-let ``Root record comes later 2`` () =
-    let t = [Branch (1, ref [Leaf 2;  Branch (3, ref [Leaf 4])])]
-
-    let treeBuilder = TreeBuilder.empty
-    treeBuilder.waitingParents.Add (0, t)
-
-    TreeBuilder.group treeBuilder 0 (Leaf 0)
-    treeBuilder.waitingParents.[0] |> should equal [Branch (0, ref [Branch (1, ref [Leaf 2;  Branch (3, ref [Leaf 4])])])]
-
-[<Fact>]
-let ``Waiting common parent`` () =
-    let t = [Leaf 1]
-
-    let treeBuilder = TreeBuilder.empty
-    treeBuilder.waitingParents.Add (10, t)
-
-    TreeBuilder.group treeBuilder 10 (Leaf 20)
-    treeBuilder.waitingParents.[10] |> should equal [Leaf 1; Leaf 20]
-
-[<Fact>]
-let ``Depth First Search`` () =
-    let t = [Branch (1, ref [Leaf 2;  Branch (3, ref [Branch (4, ref [Branch (5, ref [Leaf 6])])])]); Leaf 7; Branch (8, ref [Leaf 9])]
-
-    let treeBuilder = TreeBuilder.empty
-    treeBuilder.waitingParents.Add (10, t)
-
-    TreeBuilder.placeLeaf treeBuilder 1 (Leaf 11) |> should be True
-    TreeBuilder.placeLeaf treeBuilder 2 (Leaf 21) |> should be True
-
-    TreeBuilder.placeLeaf treeBuilder 3 (Leaf 31) |> should be True
-    TreeBuilder.placeLeaf treeBuilder 4 (Leaf 41) |> should be True
-    TreeBuilder.placeLeaf treeBuilder 5 (Leaf 51) |> should be True
-
-    TreeBuilder.placeLeaf treeBuilder 7 (Leaf 71) |> should be True
-    TreeBuilder.placeLeaf treeBuilder 9 (Leaf 91) |> should be True
-
-    TreeBuilder.placeLeaf treeBuilder 0 (Leaf 10) |> should be False
 
 [<Fact>]
 let ``One node`` () =
@@ -204,17 +21,6 @@ let ``One node`` () =
     isBranch tree |> should equal false
     recordId tree |> should equal 0
     children tree |> should be Empty
-
-[<Fact>]
-let ``Two nodes`` () =
-    let input =
-        [
-            { RecordId = 0; ParentId = 0 }
-            { RecordId = 1; ParentId = 0 }
-        ]
-    let tree = buildTree input
-
-    children tree |> should equal [(Leaf 1)]
 
 [<Fact>]
 let ``Three nodes in order`` () =
@@ -284,26 +90,6 @@ let ``More than two children`` () =
     children tree |> List.item 2 |> recordId |> should equal 3
 
 [<Fact>]
-let ``Take and attach branch in a branch`` () =
-    let input =
-        [
-            { RecordId = 5; ParentId = 1 };
-            { RecordId = 4; ParentId = 1 };
-            { RecordId = 0; ParentId = 0 };
-            { RecordId = 1; ParentId = 0 };
-        ]
-    (*
-        (0, 0)
-            - (1, 0)
-                - (4, 1)
-                - (5, 1)
-
-    *)
-
-    let tree = buildTree input
-    isBranch tree |> should equal true
-
-[<Fact>]
 let ``Binary tree`` () =
     let input =
         [
@@ -315,17 +101,6 @@ let ``Binary tree`` () =
             { RecordId = 0; ParentId = 0 };
             { RecordId = 6; ParentId = 2 }
         ]
-
-        (*
-            (0, 0)
-                - (1, 0)
-                    - (4, 1)
-                    - (5, 1)
-                - (2, 0)
-                    - (3, 2)
-                    - (6, 2)
-
-        *)
 
     let tree = buildTree input
 
@@ -365,15 +140,6 @@ let ``Unbalanced tree`` () =
             { RecordId = 0; ParentId = 0 };
             { RecordId = 6; ParentId = 2 }
         ]
-    (*
-        (0; 0)
-            - (1; 0)        // branch
-                  - (4; 1)
-            - (2; 0)        // branch
-                  - (3; 2)
-                  - (5; 2)
-                  - (6; 2)
-    *)
 
     let tree = buildTree input
 
@@ -402,43 +168,6 @@ let ``Unbalanced tree`` () =
     children tree |> List.item 1 |> children |> List.item 2 |> recordId |> should equal 6
 
 [<Fact>]
-let ``Tricky Unbalanced tree`` () =
-    let input =
-        [
-            { RecordId = 0; ParentId = 0 };
-            { RecordId = 3; ParentId = 1 }; // detached
-            { RecordId = 4; ParentId = 2 };
-            { RecordId = 2; ParentId = 1 }; // unite them
-            { RecordId = 1; ParentId = 0 };
-        ]
-    (*
-        (0; 0)
-           - (1; 0)
-               - (2; 1)
-                    - (4; 2)
-               - (3; 1)
-    *)
-    let tree = buildTree input
-    isBranch tree |> should equal true
-
-[<Fact>]
-let ``Branch id search`` () =
-    let t = Branch (1, ref [Leaf 2;  Branch (3, ref [Branch (4, ref [Branch (5, ref [Leaf 6])])])])
-
-    TreeBuilder.containsId 1 t |> should be True
-    TreeBuilder.containsId 2 t |> should be True
-    TreeBuilder.containsId 3 t |> should be True
-    TreeBuilder.containsId 4 t |> should be True
-    TreeBuilder.containsId 5 t |> should be True
-    TreeBuilder.containsId 6 t |> should be True
-
-    TreeBuilder.containsId 7 t |> should be False
-
-// Type of errors
-
-// 1. Validate input
-
-[<Fact>]
 let ``Empty input`` () =
     let input = []
     (fun () -> buildTree input |> ignore) |> should throw typeof<Exception>
@@ -451,20 +180,20 @@ let ``Root node has parent`` () =
     (fun () -> buildTree input |> ignore) |> should throw typeof<Exception>
 
 [<Fact>]
-let ``Higher id parent of lower id`` () =
-    let input =
-        [
-            { RecordId = 0; ParentId = 0 };
-            { RecordId = 2; ParentId = 0 };
-            { RecordId = 1; ParentId = 2 }
-        ]
+let ``No root node`` () =
+    let input = [ { RecordId = 1; ParentId = 0 } ]
     (fun () -> buildTree input |> ignore) |> should throw typeof<Exception>
 
-    (*
-        (0; 0)
-            - (2; 0)
-                - (1; 2)  // id should be bigger than parent id
-    *)
+[<Fact>]
+let ``Out of bound record id`` () =
+    let input =
+        [
+            { RecordId = 2; ParentId = 0 };
+            { RecordId = 4; ParentId = 2 };
+            { RecordId = 1; ParentId = 0 };
+            { RecordId = 0; ParentId = 0 }
+        ]
+    (fun () -> buildTree input |> ignore) |> should throw typeof<Exception>
 
 [<Fact>]
 let ``Cycle directly`` () =
@@ -480,18 +209,6 @@ let ``Cycle directly`` () =
         ]
     (fun () -> buildTree input |> ignore) |> should throw typeof<Exception>
 
-    (*
-        (0; 0)
-            - (1; 0)
-                - (4; 1)
-
-            (2; 2) // Parent and id of incomming should be diffrent
-            (3; 2)
-            (5; 2)
-
-            (6; 3)
-    *)
-
 [<Fact>]
 let ``Cycle indirectly`` () =
     let input =
@@ -506,35 +223,12 @@ let ``Cycle indirectly`` () =
         ]
     (fun () -> buildTree input |> ignore) |> should throw typeof<Exception>
 
-    (*
-        (0; 0)
-            - (1; 0)
-                - (4; 1)
-        (2; 6)            // waiting for node with id=6
-            - (3; 2)
-                - (6; 3)  // id not on waiting ids, parent not in leaves
-            - (5; 2)
-    *)
-
-// 2. Analyze after build
-
 [<Fact>]
-let ``No root node`` () =
-    let input = [ { RecordId = 1; ParentId = 0 } ]
-    (fun () -> buildTree input |> ignore) |> should throw typeof<Exception>
-
-
-[<Fact>]
-let ``Non-continuous`` () =
+let ``Higher id parent of lower id`` () =
     let input =
         [
+            { RecordId = 0; ParentId = 0 };
             { RecordId = 2; ParentId = 0 };
-            { RecordId = 4; ParentId = 2 };
-            { RecordId = 1; ParentId = 0 };
-            { RecordId = 0; ParentId = 0 }
+            { RecordId = 1; ParentId = 2 }
         ]
-
-    let tree = buildTree input
-    tree |> should equal (Branch (0, ref [Leaf 1; Branch (2, ref [Leaf 4])]))
-
-    (* Bug in the project was found. https://github.com/exercism/fsharp/issues/757 *)
+    (fun () -> buildTree input |> ignore) |> should throw typeof<Exception>
